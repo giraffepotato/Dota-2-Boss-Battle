656a3008572040803dd10aa38efb7b4fbd3002a7
Kyle <zkyle1337@gmail.com>
2016-08-09 10:17:02 -0700

    almost finished rexxar

--- game/dota_addons/boss_test/scripts/vscripts/rexxar_ai.lua
+++ game/dota_addons/boss_test/scripts/vscripts/rexxar_ai.lua
@@ -4,18 +4,22 @@ function Spawn(entityKeyValues)
 	ABILITY_beetle_swarm_spell = thisEntity:FindAbilityByName("beetle_swarm")
 	ABILITY_bird_flare_spell = thisEntity:FindAbilityByName("bird_flare")
 	ABILITY_unit_spawn_spell = thisEntity:FindAbilityByName("unit_spawn_test")
-	--ABILITY_enrage_huskar_spell = thisEntity:FindAbilityByName("enrage_huskar")
+	ABILITY_big_trees_spell = thisEntity:FindAbilityByName("rexxar_spawn_big_trees")
 	print("Starting AI for "..thisEntity:GetUnitName().." "..thisEntity:GetEntityIndex())
-	
+	tentacles = "no"
+	print("tentacles is "..tentacles)
+
 	Timers:CreateTimer(function()
 local index = thisEntity:GetEntityIndex()
 local healthRemaining = thisEntity:GetHealth() / thisEntity:GetMaxHealth() 
 	
-	if ABILITY_tentacle_mess_spell:IsFullyCastable() then
-		local units = FindUnitsInRadius( DOTA_TEAM_GOODGUYS, thisEntity:GetAbsOrigin(), nil, 950, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_ALL, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, FIND_ANY_ORDER, false )
+	if (ABILITY_tentacle_mess_spell:IsFullyCastable()) and (tentacles == "no") then
+		local units = FindUnitsInRadius( DOTA_TEAM_GOODGUYS, thisEntity:GetAbsOrigin(), nil, 800, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_ALL, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, FIND_ANY_ORDER, false )
 		if units ~= nil then
 			if #units >= 1 then
-				thisEntity:CastAbilityNoTarget(ABILITY_tentacle_mess_spell, index)
+				thisEntity:CastAbilityToggle(ABILITY_tentacle_mess_spell, index)
+				tentacles = "yes"
+				print("tentacles is "..tentacles)
 			end
 		end
 	end	 
@@ -42,8 +46,17 @@ local healthRemaining = thisEntity:GetHealth() / thisEntity:GetMaxHealth()
 	if ABILITY_unit_spawn_spell:IsFullyCastable() then
 		local units = FindUnitsInRadius( DOTA_TEAM_GOODGUYS, thisEntity:GetAbsOrigin(), nil, 1000, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false )
 		if units ~= nil then
-			if #units >= 1 then
+			if #units >= 2 then
 				thisEntity:CastAbilityNoTarget(ABILITY_unit_spawn_spell, index)
+			end
+		end
+	end
+
+	if ABILITY_big_trees_spell:IsFullyCastable() then
+		local units = FindUnitsInRadius( DOTA_TEAM_GOODGUYS, thisEntity:GetAbsOrigin(), nil, 1000, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false )
+		if units ~= nil then
+			if #units >= 2 then
+				thisEntity:CastAbilityNoTarget(ABILITY_big_trees_spell, index)
 			end
 		end
 	end
