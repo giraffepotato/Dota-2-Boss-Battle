d1ff2ae6a24f4976bf66f765eab2a24f6a069f23
Kyle <zkyle1337@gmail.com>
2016-08-15 12:51:30 -0700

    frogs rain

--- game/dota_addons/boss_test/scripts/vscripts/control_me.lua
+++ game/dota_addons/boss_test/scripts/vscripts/control_me.lua
@@ -1,5 +1,12 @@
 function Spawn( entityKeyValues )
+	Timers:CreateTimer(function()
 	units = FindUnitsInRadius( DOTA_TEAM_GOODGUYS, thisEntity:GetAbsOrigin(), nil, 9999999, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, FIND_ANY_ORDER, false )
-	units[1]:GetEntityIndex() = first_player_index
-	thisEntity:SetControllableByPlayer(first_player_index, true)
+	for i=1,#units,1 do
+			if units ~= nil and i_unit ~= nil then
+				first_player_index = units[i]:GetEntityIndex()
+			end
+	end
+	thisEntity:SetControllableByPlayer(first_player_index, true
+		return 0.5
+	end)
 end
