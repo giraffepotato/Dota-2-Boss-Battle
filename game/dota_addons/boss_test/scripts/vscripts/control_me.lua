e2bdef4894b9961f98a58ec8556a0332a9239816
Kyle <zkyle1337@gmail.com>
2016-08-15 22:10:41 -0700

    frogs rain

--- game/dota_addons/boss_test/scripts/vscripts/control_me.lua
+++ game/dota_addons/boss_test/scripts/vscripts/control_me.lua
@@ -1,12 +1,16 @@
-function Spawn( entityKeyValues )
+--what the hell am i doing
+--[[function Spawn( entityKeyValues )
 	Timers:CreateTimer(function()
 	units = FindUnitsInRadius( DOTA_TEAM_GOODGUYS, thisEntity:GetAbsOrigin(), nil, 9999999, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, FIND_ANY_ORDER, false )
 	for i=1,#units,1 do
-			if units ~= nil and i_unit ~= nil then
+		i_unit = units[i]:GetUnitName()
+		i_unit_team_num = i_unit:GetTeamNumber
+			if units ~= nil and (i_unit:GetTeamName(i_unit_team_num) == "DOTA_TEAM_GOODGUYS") then
 				first_player_index = units[i]:GetEntityIndex()
+				thisEntity:SetControllableByPlayer(first_player_index, true)
+				print("set controllable by "..units[i]:GetUnitName())
 			end
 	end
-	thisEntity:SetControllableByPlayer(first_player_index, true
 		return 0.5
 	end)
 end
--- game/dota_addons/boss_test/scripts/vscripts/rexxar_ai.lua
+++ game/dota_addons/boss_test/scripts/vscripts/rexxar_ai.lua
@@ -7,7 +7,6 @@ function Spawn(entityKeyValues)
 	ABILITY_big_trees_spell = thisEntity:FindAbilityByName("rexxar_spawn_big_trees")
 	print("Starting AI for "..thisEntity:GetUnitName().." "..thisEntity:GetEntityIndex())
 	tentacles = "no"
-	print("tentacles is "..tentacles)
 
 	Timers:CreateTimer(function()
 local index = thisEntity:GetEntityIndex()
@@ -19,13 +18,11 @@ local healthRemaining = thisEntity:GetHealth() / thisEntity:GetMaxHealth()
 			if #units >= 1 then
 				thisEntity:CastAbilityToggle(ABILITY_tentacle_mess_spell, index)
 				tentacles = "yes"
-				print("tentacles is "..tentacles)
 			end
 		end
 	end	 
 	
 	if ABILITY_beetle_swarm_spell:IsFullyCastable() then
-		print("beetle castable")
 		local units = FindUnitsInRadius( DOTA_TEAM_GOODGUYS, thisEntity:GetAbsOrigin(), nil, 1000, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_ALL, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, FIND_ANY_ORDER, false )
 		if units ~= nil then
 			if #units >= 1 then
@@ -35,7 +32,6 @@ local healthRemaining = thisEntity:GetHealth() / thisEntity:GetMaxHealth()
 	end	 
 
 	if ABILITY_bird_flare_spell:IsFullyCastable() then
-		print("flare castable")
 		local units = FindUnitsInRadius( DOTA_TEAM_GOODGUYS, thisEntity:GetAbsOrigin(), nil, 1000, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false )
 		if units ~= nil then
 			if #units >= 1 then
@@ -47,7 +43,6 @@ local healthRemaining = thisEntity:GetHealth() / thisEntity:GetMaxHealth()
 
 	if ABILITY_unit_spawn_spell:IsFullyCastable() then
 		local units = FindUnitsInRadius( DOTA_TEAM_GOODGUYS, thisEntity:GetAbsOrigin(), nil, 1500, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false )
-		print("spawn small trees castable "..#units)
 		if units ~= nil then
 			if #units >= 2 then
 				thisEntity:CastAbilityNoTarget(ABILITY_unit_spawn_spell, index)
@@ -57,7 +52,6 @@ local healthRemaining = thisEntity:GetHealth() / thisEntity:GetMaxHealth()
 
 	if ABILITY_big_trees_spell:IsFullyCastable() then
 		local units = FindUnitsInRadius( DOTA_TEAM_GOODGUYS, thisEntity:GetAbsOrigin(), nil, 1500, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false )
-		print("big trees castable "..#units)
 		if units ~= nil then
 			if #units >= 2 then
 				thisEntity:CastAbilityNoTarget(ABILITY_big_trees_spell, index)
